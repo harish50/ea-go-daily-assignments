03b085805893f7565a2d81d0a539dd94a4174213
Harish S <harish@everest.engineering>
2022-12-21 21:13:38 +0530

    Day-1 - Calculator: Add subtraction functionality

--- day-1-calculator/calculator.go
+++ day-1-calculator/calculator.go
@@ -6,3 +6,7 @@ type Calculator struct {
 func addition(num1 float64, num2 float64) float64 {
 	return num1 + num2
 }
+
+func subtraction(num1 float64, num2 float64) float64 {
+	return num1 - num2
+}
--- day-1-calculator/calculator_test.go
+++ day-1-calculator/calculator_test.go
@@ -8,3 +8,7 @@ import (
 func TestAddition(t *testing.T) {
 	assert.Equal(t, addition(1, 2), float64(3))
 }
+
+func TestSubtraction(t *testing.T) {
+	assert.Equal(t, subtraction(2, 3), float64(-1))
+}
